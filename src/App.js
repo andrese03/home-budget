f2f1cb9c4407ff0ae13a9149ae4c68793c928978
Andrés Encarnación Ortiz <andresencarnacion03@gmail.com>
2018-04-20 02:47:15 -0400

    Cleaning code, added font awesome,

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React, { Component } from 'react';
 import ExpenseContainer from './expenses/ExpenseContainer';
 
 import 'bootstrap3/dist/css/bootstrap.css';
+import fontawesome from '@fortawesome/fontawesome'
+import brands from '@fortawesome/fontawesome-free-brands'
+import solid from '@fortawesome/fontawesome-free-solid'
+
+fontawesome.library.add(brands, solid);;
 
 class App extends Component {
 	state = {
--- src/expenses/ExpenseContainer.jsx
+++ src/expenses/ExpenseContainer.jsx
@@ -1,4 +1,5 @@
 import React,  { Component, Fragment } from 'react';
+import Icon from '@fortawesome/react-fontawesome'
 import ExpenseForm from './ExpenseFormContainer';
 import ExpenseList from './ExpenseListContainer';
 
@@ -7,14 +8,14 @@ class ExpenseContainer extends Component {
 	render() {
 		return (
 			<Fragment>
-				<h1>Gastos <small>Registre los gastos que hace día a día</small></h1>
+				<h1><Icon icon="coffee" /> Gastos <small>Registre los gastos que hace día a día</small></h1>
 				<ExpenseForm onSubmit={this.createExpense}></ExpenseForm>
 				<h1>Listado de Gastos<small> No tengas verguenza, ya lo hiciste</small></h1>
 				<ExpenseList></ExpenseList>
 			</Fragment>
 		)
 	}
-	
+
 }
 
 export default ExpenseContainer;
