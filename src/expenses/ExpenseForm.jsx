723af2fc0cc9b30e0c8a0a8522b9c4514cf76d56
Andrés Encarnación Ortiz <andresencarnacion03@gmail.com>
2018-05-04 15:54:04 -0400

    Improvements to Expense form

--- src/expenses/ExpenseForm.jsx
+++ src/expenses/ExpenseForm.jsx
@@ -8,7 +8,17 @@ class ExpenseForm extends Component {
 		super(props);
 		
 		// State Definitions
-		this.state = this.props.expense || { createdAt: '', amount: '', description: '', };
+		this.state = this.props.expense || 
+		{ 
+			createdAt: '', 
+			amount: '', 
+			description: '', 
+			paymentType: '', 
+			paymentTypes: [
+				{ label: 'Efectivo', value: 'cash' },
+				{ label: 'Tarjeta', value: 'card'}
+			]
+		};
 
 		// Bindings
 		this.onSubmit = this.onSubmit.bind(this);
@@ -21,8 +31,11 @@ class ExpenseForm extends Component {
 
 	onSubmit(event) {
 		event.preventDefault();
-		const { createdAt, amount, description } = this.state;
-		this.props.createExpense({ createdAt, amount, description });
+		const { createdAt, amount, description, paymentType, card } = this.state;
+		const expense = { createdAt, amount, description, paymentType, card };
+		if (paymentType != 'card')
+			delete expense.card;
+		this.props.createExpense(expense);
 	}
 
 	static getDerivedStateFromProps(nextProps, prevState) {
@@ -44,8 +57,8 @@ class ExpenseForm extends Component {
 	render() {
 
 		return (
-			<form onSubmit={this.onSubmit}>
-				<div className="form-group">
+			<form onSubmit={this.onSubmit} id="expenseForm">
+				<div className={'form-group' + this.state.errors.createdAt.required ? 'hasError' : ''}>
 					<label htmlFor="createdAt">Fecha</label>
 					<input type="date" className="form-control" id="createdAt" name="createdAt" value={this.state.createdAt} onChange={this.onChange} autoFocus="true" required/>
 				</div>
@@ -57,7 +70,23 @@ class ExpenseForm extends Component {
 					<label htmlFor="description">Descripción</label>
 					<input type="text" className="form-control" id="description" name="description" value={this.state.description} onChange={this.onChange} placeholder="Ej. Comer pizza" required/>
 				</div>
-				<button type="submit" className="btn btn-primary">Agregar</button>
+				<div className="form-group">
+					<label htmlFor="description">Pagado con...</label>
+					<select name="paymentType" id="paymentType" className="form-control" value={this.state.paymentType} onChange={this.onChange}>
+						<option value=""></option>
+						{this.state.paymentTypes.map((x, i) => (
+							<option key={i} value={x.value}>{x.label}</option>
+						))}
+					</select>
+				</div>
+				{
+					this.state.paymentType == 'card' &&
+					<div className="form-group">
+						<label htmlFor="description">Tarjeta</label>
+						<input type="text" className="form-control" id="card" name="card" value={this.state.card} onChange={this.onChange} required />
+					</div>
+				}
+					<button type="submit" className="btn btn-primary">Agregar</button>
 			</form>
 		)
 	}
